54f3e1d78fbffb11a880c6aaf5c2dd27a5f43643
James Magee <james@jamess-air.myfiosgateway.com>
2026-05-05 19:18:57 -0400

    disemvowel troll and binary addition

---
+++ CodeWars/7kyu.js
@@ -0,0 +1,28 @@
+// disemvowel trolls
+function disemvowel(string) {
+    let result = string.replace(/[aeiou]/gi, "");
+    return result;
+}
+
+
+
+
+
+// binary addition
+function toBinary(a, b) {
+    let sum = (a + b).toString(2);
+    return sum;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
