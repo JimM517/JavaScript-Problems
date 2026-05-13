fa74a987491ef85cbbb8bf805bf20cef6df91a68
James Magee <james@Jamess-MacBook-Air.local>
2026-05-12 20:46:00 -0400

    moving zeros to the end

--- CodeWars/5kyu.js
+++ CodeWars/5kyu.js
@@ -197,7 +197,24 @@ const solution = function(firstArray, secondArray) {
 
 
 
+// moving zeros to the end
+function moveZeros(arr) {
+  
+   let k = 0;
 
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 0) {
+            arr[k++] = arr[i];
+        }
+    }
+
+    while (k < arr.length) {
+        arr[k++] = 0;
+    }
+
+    return arr;
+
+}
 
 
 
