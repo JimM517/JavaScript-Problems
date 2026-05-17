67e38fe39cd6969d64ccff722ce1dec77d17c442
James Magee <james@Jamess-MacBook-Air.local>
2026-05-17 08:43:06 -0400

    reverse sequence

--- CodeWars/8kyu.js
+++ CodeWars/8kyu.js
@@ -115,6 +115,15 @@ function find(a,e) {
 }
 
 
+// reversed sequence
+const reverseSeq = n => {
+    let res = []
+    for (let i = n; i >= 1; i--) {
+      res.push(i);
+    }
+    return res;
+}
+
 
 
 
