9f838990cf50f62548135d4132245f4ae36e5023
James Magee <james@Jamess-MacBook-Air.local>
2026-05-17 16:38:50 -0400

    is a number prime?

--- CodeWars/6kyu.js
+++ CodeWars/6kyu.js
@@ -108,7 +108,23 @@ function isPangram(string) {
 
 
 
+// is a number prime?
+function isPrime(num) {
 
+   if (num < 2) return false;
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+
+
+
+
+}
 
 
 
